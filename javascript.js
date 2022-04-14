ec97f70530529c85611ea17201a37b4283ece440
Oyebode Oluwatobi <oyebodeoyewale065@gmail.com>
2022-04-14 16:29:56 +0100

    first commit

---
+++ javascript.js
@@ -0,0 +1,17 @@
+function myfunction() {
+
+    var email= document.getElementById('email').value;
+    var password = document.getElementById('password').value;
+
+    if ( email== "admin" && password=="1234" )
+    {
+        alert("login sucesfully");
+
+    } 
+    else {
+        alert(" invalid Username and password")    
+    } 
+
+}
+    
+
